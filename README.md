818f8ce293f1275230c39a62a31df3531cc821c9
eng-Alaa-Awwad <se.alaaawwad@gmail.com>
2025-10-18 10:22:29 +0300

    Initial commit: Added Chapter folders and solutions

---
+++ README.md
@@ -0,0 +1,7 @@
+# Introduction to Programming with C++ (3rd Edition) – Solutions
+
+This repository contains my solutions to exercises from  
+*Introduction to Programming with C++ (3rd Edition, International Edition)*  
+by Y. Daniel Liang and Mohit P. Tahiliani.
+
+Each chapter has its own folder containing `.cpp` solutions.
---
+++ chapter-1/Exercises1.1.cpp
@@ -0,0 +1,15 @@
+// ******** QUESTION ******** 
+/*
+(Display two messages) Write a program that displays Introduction to Computers and Welcome to Object-Oriented Programming.
+*/
+
+// ******** SOLUTION ******** 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+  cout << "Introduction to Computers" << endl;
+  cout << "Welcome to Object-Oriented Programming" << endl;
+  return 0;
+}
